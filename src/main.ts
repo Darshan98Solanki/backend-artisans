8037e5bc55205345eaad3eb48599666382f5abba
Darshan98Solanki <106055701+Darshan98Solanki@users.noreply.github.com>
2026-02-14 18:14:54 +0530

    allow all origin with all methods

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // for the demo version. allow all origins. In production, you should specify the allowed origins.
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  }); // for the demo version. allow all origins. In production, you should specify the allowed origins.
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
