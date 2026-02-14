994664424a9578935f466d058a5a4eb2b466033c
Darshan98Solanki <106055701+Darshan98Solanki@users.noreply.github.com>
2026-02-14 18:07:31 +0530

    add cors enable

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors(); // for the demo version. allow all origins. In production, you should specify the allowed origins.
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
